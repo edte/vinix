19de7e3313ee8631bac4dcfbc9d4b4ef407eeb1c
edte <zzzzip6@gmail.com>
2023-05-01 17:41:26 +0800

    feat(*): enter long mode

--- kernel.asm
+++ kernel.asm
@@ -3,29 +3,49 @@ org PHYSICAL_KERNEL_ADDR
 global _start	
 
 [section .text]	
+ALIGN	64
+[bits 64]
 _start:	
-    push EnterKernelMessage
-    jmp $
+    mov ebx, EnterKernelMessage
+    mov eax,10
+    mov ecx,0
+    call DispStr64
 
-;*************************************************************
-DispStr32:
-    push ebp
-    mov ebp, esp
-    mov ebx, [ebp+8]        ; [ebp+8]即传入的字符串参数
-    xor ecx, ecx
-    mov ah, 0x0f            ; ah为打印的颜色属性，0x0f为白字黑底
-    mov edx, 0xb8000+160*5    ; 显存的地址
-loop1_begin:
+Pause:
+    hlt
+    jmp Pause
+
+
+;************************************************************8
+; 函数： 在长模式下打印字符串 
+; 参数: 
+;       rbx: 字符串首地址
+;       rax: 行数
+;       rcx, 列数
+;************************************************************8
+DispStr64:
+    xor rcx, rcx            ; 清空 ecx
+    
+    mov rdx,80
+    mul rdx
+    add rax, rcx
+    mov rdx,2
+    mul rdx
+    add rax,0xb8000
+    mov rdx, rax            ; 显存的地址
+
+    mov ah, 0x0f            ; ah 为打印的颜色属性，0x0f为白字黑底
+loop1_begin64:
     mov al, [ebx]           ; al为被打印的字符
     cmp al, 0               ; 若al为0，结束打印
-    je loop1_end
+    je loop1_end64
     mov [edx], ax           ; 向显存中写入字符及其颜色属性（2字节）
     inc ebx
     add edx, 2
-    jmp loop1_begin
-loop1_end:
-    mov esp, ebp
-    pop ebp
+    jmp loop1_begin64
+loop1_end64:
     ret
 
+
+[SECTION .data64]
 EnterKernelMessage db      "enter kernel", 0 
--- loader.asm
+++ loader.asm
@@ -15,9 +15,8 @@ _start:
     call DispStr16		
 
 
-
     ; [step 2] 显示开始 load 日志
-    mov si,PrepareMessage			
+    mov si,LoadKernelToMemMessage			
     mov dh,2				;设置显示行
 	mov dl,0				;设置显示列
     call DispStr16		
@@ -41,7 +40,7 @@ _start:
     mov ecx, 10   	                ; 读取100个扇区
     call ReadLBA16
 
-    ; 把 cr0 的最低位置为 1，开启保护模式
+    ; [step 5] 把 cr0 的最低位置为 1，开启保护模式
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
@@ -64,10 +63,10 @@ DispStr16:
             mov al,[bx]			;读取1个字节到al
             inc bx				;读取下个字节
             cmp al,0			;是否以0结束
-            je .sDispStr32
+            je .sDispStr
             inc	cx				;计数器
             jmp .len
-    .sDispStr32:;显示字符串
+    .sDispStr:;显示字符串
             mov bx,si
             mov bp,bx
             mov bx,ds
@@ -151,10 +150,9 @@ ReadLBA16:
       ret
 
 
-
 [SECTION .data16]
 StartLoaderMessage:	            db	"Start Loader",0
-PrepareMessage:	                db	"load kernel succ",0
+LoadKernelToMemMessage:	        db	"load kernel to memery succ",0
 
 
 ;===================================================================================================================================
@@ -163,8 +161,7 @@ PrepareMessage:	                db	"load kernel succ",0
 gdt_start:
 ; 第一个描述符必须是空描述符
 gdt_null:
-    dd 0
-    dd 0
+    dq 0
 ; 代码段描述符
 gdt_code:
     dw 0xffff    ; Limit (bits 0-15)
@@ -210,117 +207,92 @@ _start32:
     mov es, ax
     mov fs, ax          ; fs和gs寄存器由操作系统使用，这里统一设成0x10
     mov gs, ax
-    mov ax, SelectorStack32        ; 将栈段寄存器ss置为0x18
+    mov ax, SelectorData32        ; 将栈段寄存器ss置为0x18
     mov ss, ax
     mov ebp, BASE_OF_STACK     ; 现在栈顶指向 0x7c00
     mov esp, ebp
- 
+
     ; [step 2] 打印进入保护模式日志
-    push EnterProtectMessage             ; 被打印字符的地址
+    mov ebx, EnterProtectMessage             ; 被打印字符的地址
+    mov ax, 3
+    mov cx,0
     call DispStr32
  
-
-    ; [step 3] 进入长模式
-	mov	ax,	0x10
-	mov	ds,	ax
-	mov	es,	ax
-	mov	fs,	ax
-	mov	ss,	ax
-
-	mov	esp,	7E00h
-
-	call	enter_long_mode
+    ; [step 3] 测试是否支持长模式 
+	call	if_enter_long_mode
 	test	eax,	eax
-
 	jz	Pause
 
-	; call	SetupPaging
-	
-	; jmp	SelectorCode64:PHYSICAL_KERNEL_ADDR
-
-
-;=======	init template page table 0x90000 make sure there is not dirty data
-
+    ; [step 4] 初始化临时页表
 	mov	dword	[0x90000],	0x91007
 	mov	dword	[0x90800],	0x91007		
-
 	mov	dword	[0x91000],	0x92007
-
 	mov	dword	[0x92000],	0x000083
-
 	mov	dword	[0x92008],	0x200083
-
 	mov	dword	[0x92010],	0x400083
-
 	mov	dword	[0x92018],	0x600083
-
 	mov	dword	[0x92020],	0x800083
-
 	mov	dword	[0x92028],	0xa00083
 
-;=======	load GDTR
 
-	; db	66h
-	; lgdt	[GdtPtr64]
-	; 
-	; mov	ax,	0x10
-	; mov	ds,	ax
-	; mov	es,	ax
-	; mov	fs,	ax
-	; mov	gs,	ax
-	; mov	ss,	ax
-
-	mov	esp,	7E00h
+    ; [step 5] 加载 GDT
+	db	66h
+	lgdt	[Gdt64Ptr]
 
 
-    ; [step ] 开启 PAE 功能	
+    ; [step 6] 开启 PAE 功能	
 	mov	eax,	cr4
 	bts	eax,	5
 	mov	cr4,	eax
 
 	
-    ; [step ] 加载页目录基地址
+    ; [step 7] 加载页目录基地址
 	mov	eax,	0x90000
 	mov	cr3,	eax
 	
 
-    ; [step ] 开启长模式/ IA-32e
+    ; [step 8] 开启长模式/ IA-32e
 	mov	ecx,	0C0000080h		;IA32_EFER
 	rdmsr
 	bts	eax,	8
 	wrmsr
 	
-	
-    ; [step ] 开启分页机制
+
+    ; [step 9] 开启分页机制
 	mov	eax,	cr0
 	bts	eax,	0
 	bts	eax,	31
 	mov	cr0,	eax
 
+    mov ebx, PrepareEnterLongModeMessage            ; 被打印字符的地址
+    mov ax, 4
+    mov cx,0
+    call DispStr32
+
+    ; [step 10]  跳转进入临时长模式
 	jmp	SelectorCode64:_start64
 
 Pause:
     hlt
+
     jmp Pause
 
 
 ;************************************************************8
-; 函数： 进入长模式
+; 函数： 测试是否支持进入长模式
 ; 无参数
 ;************************************************************8
-enter_long_mode:
-
+if_enter_long_mode:
 	mov	eax,	0x80000000
 	cpuid
 	cmp	eax,	0x80000001
 	setnb	al	
-	jb	enter_long_mode_done
+	jb	if_enter_long_mode_done
 	mov	eax,	0x80000001
 	cpuid
 	bt	edx,	29
 	setc	al
-enter_long_mode_done:
-	
+if_enter_long_mode_done:
 	movzx	eax,	al
 	ret
 
@@ -336,25 +308,39 @@ PrintA20:
     mov eax, [esi]
     cmp eax, edi
     je A20_DISABLE
-    push aA20_ENABLE
+
+    mov ebx, aA20_ENABLE
+    mov ax,0
+    mov cx,0
     call DispStr32
 A20_DISABLE:
-    push aA20_DISABLE
+    mov ebx, aA20_DISABLE
+    mov ax,0
+    mov cx,0
     call DispStr32
 
     ret
  
+
 ;************************************************************8
 ; 函数： 在保护模式下打印字符串 
-; 参数: push 字符串首地址
+; 参数: 
+;       ebx: 字符串首地址
+;       eax: 行数
+;       ecx, 列数
 ;************************************************************8
 DispStr32:
-    push ebp
-    mov ebp, esp
-    mov ebx, [ebp+8]        ; [ebp+8]即传入的字符串参数
-    xor ecx, ecx
-    mov ah, 0x0f            ; ah为打印的颜色属性，0x0f为白字黑底
-    mov edx, 0xb8000+160*3    ; 显存的地址
+    xor ecx, ecx            ; 清空 ecx
+    
+    mov edx,80
+    mul edx
+    add eax, ecx
+    mov edx,2
+    mul edx
+    add eax,0xb8000
+    mov edx, eax            ; 显存的地址
+
+    mov ah, 0x0f            ; ah 为打印的颜色属性，0x0f为白字黑底
 loop1_begin:
     mov al, [ebx]           ; al为被打印的字符
     cmp al, 0               ; 若al为0，结束打印
@@ -364,55 +350,29 @@ loop1_begin:
     add edx, 2
     jmp loop1_begin
 loop1_end:
-    mov esp, ebp
-    pop ebp
     ret
 
 [SECTION .data32]
-aA20_ENABLE         db      "A20 Enable", 0
-aA20_DISABLE        db      "A20 Disable", 0
-EnterProtectMessage db      "enter protect model", 0 
+aA20_ENABLE                 db      "A20 Enable", 0
+aA20_DISABLE                db      "A20 Disable", 0
+EnterProtectMessage         db      "enter protect model", 0 
+PrepareEnterLongModeMessage db      "Prepare enter long mode message",0 
+PauseMessage                db      "pause",0
+
 
 ;===================================================================================================================================
 ; 64 位 GDT
 [SECTION .gdt64]
-
-LABEL_GDT64:		dq	0x0000000000000000
-LABEL_DESC_CODE64:	dq	0x0020980000000000
-LABEL_DESC_DATA64:	dq	0x0000920000000000
-
-
-
-
 gdt64_start:
 ; 第一个描述符必须是空描述符
 gdt64_null:
-    dd 0
-    dd 0
+    dq	0x0000000000000000
 ; 代码段描述符
 gdt64_code:
-    dw 0x0       ; Limit (bits 0-15)， 忽略
-    dw 0x0       ; Base (bits 0-15) ， 忽略
-    db 0x0       ; Base (bits 16-23) ， 忽略
-    db 10011010b ; Access Byte
-    db 11001111b ; Flags , Limit (bits 16-19)
-    db 0x0       ; Base (bits 24-31)， 忽略
+    dq	0x0020980000000000
 ; 数据段描述符
 gdt64_data:
-    dw 0x0       ; Limit (bits 0-15)          
-    dw 0x0       ; Base (bits 0-15)
-    db 0x0       ; Base (bits 16-23)
-    db 10010010b ; Access Byte
-    db 11001111b ; Flags , Limit (bits 16-19)
-    db 0x0       ; Base (bits 24-31)
-; 栈段描述符
-gdt64_stack:
-    dw 0x0       ; Limit (bits 0-15)           
-    dw 0x0       ; Base (bits 0-15)
-    db 0x0       ; Base (bits 16-23)
-    db 10010010b ; Access Byte
-    db 01000000b ; Flags , Limit (bits 16-19)
-    db 0x0       ; Base (bits 24-31)
+    dq	0x0000920000000000
 gdt64_end:
 
 Gdt64Len	equ	$ - gdt64_start
@@ -421,25 +381,63 @@ Gdt64Ptr	dw	Gdt64Len - 1
 
 SelectorCode64	equ	gdt64_code - gdt64_start
 SelectorData64	equ	gdt64_data - gdt64_start
-SelectorStack64	equ	gdt64_stack - gdt64_start
-
 
 [SECTION .code64]
 ALIGN	64
 [bits 64]
 _start64:
-    mov ax, SelectorCode64   ; 设置数据段选择子
-    mov ds, ax
+    ; [step 1] 初始化寄存器
+	mov	ax, SelectorData64	
+	mov	ds,	ax
+	mov	es,	ax
+	mov	fs,	ax
+	mov	ss,	ax
+	mov	esp,BASE_OF_STACK
 
+    ; [step 2] 打印日志，进入 64 位
+    mov ebx, EnterLongModeMessage
+    mov eax, 5
+    mov ecx, 0
+    call DispStr64
+
+    jmp PHYSICAL_KERNEL_ADDR
+
+    ; jmp Pause64
+
+Pause64:
+    hlt
+    jmp Pause64
+
+;************************************************************8
+; 函数： 在长模式下打印字符串 
+; 参数: 
+;       rbx: 字符串首地址
+;       rax: 行数
+;       rcx, 列数
+;************************************************************8
+DispStr64:
+    xor rcx, rcx            ; 清空 ecx
     
-    mov ax, SelectorStack64
-    mov ss,ax
-    mov rsp,BASE_OF_STACK 
+    mov rdx,80
+    mul rdx
+    add rax, rcx
+    mov rdx,2
+    mul rdx
+    add rax,0xb8000
+    mov rdx, rax            ; 显存的地址
 
-
-
-
+    mov ah, 0x0f            ; ah 为打印的颜色属性，0x0f为白字黑底
+loop1_begin64:
+    mov al, [ebx]           ; al为被打印的字符
+    cmp al, 0               ; 若al为0，结束打印
+    je loop1_end64
+    mov [edx], ax           ; 向显存中写入字符及其颜色属性（2字节）
+    inc ebx
+    add edx, 2
+    jmp loop1_begin64
+loop1_end64:
+    ret
 
 
 [SECTION .data64]
-
+EnterLongModeMessage         db      "enter long model", 0 
